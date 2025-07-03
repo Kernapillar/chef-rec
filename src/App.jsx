9899e25b1b6ccfc1c59aabb0ff467928f5d1a565
Alexander Kern <alexander.t.kern@gmail.com>
2025-07-02 22:25:25 -0500

    Ingredients input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './header'
+import Header from './components/header'
+import IngredientInput from './components/IngredientInput'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <IngredientInput/>
     </>
   )
 }
---
+++ src/components/IngredientInput.jsx
@@ -0,0 +1,10 @@
+
+
+export default function IngredientInput(){
+    return (
+        <div className="input-container">
+            <input type="text" placeholder="  e.g garlic" />
+            <button>+ Add ingredient</button>
+        </div>
+    )
+}
