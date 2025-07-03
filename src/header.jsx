c68e92d47e76a94a455ff38985eaa113f70e13dd
Alexander Kern <alexander.t.kern@gmail.com>
2025-07-02 22:05:11 -0500

    import path for image

--- src/header.jsx
+++ src/header.jsx
@@ -1,9 +1,9 @@
+import ChefIcon from "./assets/chef-icon.png"
 
 export default function Header() {
-    
     return (
         <header className="header-container">
-            <img src="./src/assets/chef-icon.png" alt="chef-guy" className="header-icon" />
+            <img src={ChefIcon} alt="chef-guy" className="header-icon" />
             <p className="header-title">Chef Reque</p>
         </header>
     ) 
