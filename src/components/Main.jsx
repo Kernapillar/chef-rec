e1002bf2e7416a85a6923df6488eebffde3eb629
Alexander Kern <alexander.t.kern@gmail.com>
2025-07-03 13:19:59 -0500

    ingredients list and styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,29 @@
 
 
 export default function Main(){
+
+    const data = ["chicken", "onion", "bell pepper", "garlic", "Steamed Rice" ]
+
+    const showIngredients = (ingredients) => {
+        return (
+            ingredients.map((ing, i) => <li className="ingredients-list-item" key={i}>{ing}</li>)
+        )
+    }
+
+    const logging = ()=>{
+        console.log("Ive been clicked")
+    }
+
     return (
         <main>
-            <form className="input-container">
+            <form className="input-container" >
                 <input type="text" placeholder="e.g garlic" aria-label="Add ingredient" />
-                <button>+ Add ingredient</button>
+                <button onClick={logging}>+ Add ingredient</button>
             </form>
+
+            <ul>
+                {showIngredients(data)}
+            </ul>
         </main>
     )
 }
