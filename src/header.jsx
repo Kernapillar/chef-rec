e955ef9cf07716ff17f71cafc76fa762b6c85ba7
Alexander Kern <alexander.t.kern@gmail.com>
2025-07-02 21:54:57 -0500

    simple header component

---
+++ src/header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    
+    return (
+        <header className="header-container">
+            <img src="./src/assets/chef-icon.png" alt="chef-guy" className="header-icon" />
+            <p className="header-title">Chef Reque</p>
+        </header>
+    ) 
+}
