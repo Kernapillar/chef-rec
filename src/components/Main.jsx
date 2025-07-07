a03a33f25bf2b9118c316fe92660789c567adf74
Alexander Kern <alexander.t.kern@gmail.com>
2025-07-06 21:15:56 -0500

    useState updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,31 +2,27 @@ import React from "react"
 
 export default function Main(){
 
-    const ingredients = ["chicken", "onion", "bell pepper", "garlic", "Steamed Rice" ]
-    const result = React.useState(ingredients)
+    const [ingredients, setIngredients]= React.useState([])
 
     const ingredientsList = ingredients.map(
         (ing, i) => <li className="ingredients-list-item" key={i}>{ing}</li>
     )
 
-    const logging = ()=>{
-        console.log("Ive been clicked")
-    }
-
     function handleSubmit(event) {
         event.preventDefault()
-        console.log("Form Submitted")
         const formData = new FormData(event.currentTarget)
         const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log(ingredients)
+        if (newIngredient.trim() != "") {
+            setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+        } 
+        event.target.reset()
     }
 
     return (
         <main>
             <form className="input-container" onSubmit={handleSubmit}>
                 <input type="text" placeholder="e.g garlic" aria-label="Add ingredient" name="ingredient"/>
-                <button onClick={logging}>+ Add ingredient</button>
+                <button type="submit">+ Add ingredient</button>
             </form>
             
             <ul className="ingredients-list">
