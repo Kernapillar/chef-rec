8e448e99a5f283c1b6513df86c56a28175216b2f
Alexander Kern <alexander.t.kern@gmail.com>
2025-08-16 23:45:01 -0500

    Boot.dev carryover

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -15,7 +15,6 @@ export default function Recipe() {
                     <li>2 tablespoons tomato paste</li>
                     <li>1 (28 oz) can crushed tomatoes</li>
                     <li>1 cup beef broth</li>
-                    <li>1 teaspoon dried oregano</li>
                     <li>1 teaspoon dried basil</li>
                     <li>Salt and pepper to taste</li>
                     <li>8 oz pasta of your choice (e.g., spaghetti, penne, or linguine)</li>
